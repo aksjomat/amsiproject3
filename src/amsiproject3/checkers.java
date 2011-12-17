43d6c3651cf94d3748f1d057596e2060626579c7
aksjomat <kamil.brozyc@uj.edu.pl>
2011-12-17 23:11:36 +0100

    komit3 - okno, 2 buttony, button zasady gry - działa...

--- src/amsiproject3/checkers.java
+++ src/amsiproject3/checkers.java
@@ -10,7 +10,7 @@ public class checkers {
        okno OknoGlowne = new okno("Warcaby",640,480);
     }
 }
-class okno extends Frame 
+class okno extends Frame implements ActionListener
 {
     	Button bNowa;
         Button bZasad;
@@ -48,8 +48,43 @@ class okno extends Frame
 		bZasad = new Button("Zasady gry");
 		bZasad.setSize(100,25);
 		bZasad.setLocation(450,145);	
+                bZasad.addActionListener(this);	
 	        add(bZasad);	    
+                
        //bez tego nie wyświetlimy okna - służy do wyświetlania okna
        show();
+	}
+        public void actionPerformed(ActionEvent ev)
+	{
+		Object 	cel = ev.getSource();
+		if (cel == bZasad)
+		{
+ 
+                   TextArea pole1 = new TextArea();// nowy obiekt klasy TextField
+                   pole1.setBounds(10,30,400,440); // ustawienie położenia komponentu w oknie aplikacji
+                   String s = "Zasady gry\n"+
+                              "1. Wykonywanie posunięć:\n"+
+                              "# Pierwszy ruch wykonuje zawodnik grający białymi.\n"+
+                              "# Gracze wykonują posunięcia po jednym ruchu, \nna przemian, własnymi warcabami.\n"+
+                              "# Wykonanie ruchu jest obowiązkowe.\n"+
+                              "# Kamień porusza się po przekątnej tylko do przodu\n na wolne pole następnej linii.\n"+
+                              "# W sytuacji, gdy kamień staje na pole przemiany \n(dla białych to pola: b8, d8, f8, h8; dla czarnych: \na1, c1, e1, g1) staje się damką.\n"+
+                              "# Damka porusza się po przekątnych we wszystkich \nkierunkach na dowolne wolne pole.\n"+
+                              "\n"+
+                              "2. Bicie:\n"+
+                              "# Jeżeli kamień znajduje się w sąsiedztwie po \nprzekątnej przeciwnika, za którym jest wolne pole, \nto może on przeskoczyć ten kamień i zająć to wolne \npole. Kamień, który został przeskoczony, zostaje\n usunięty.\n"+
+                              "# Bicie wykonuje się do przodu lub do tyłu.\n"+
+                              "# Bicie jest obowiązkowe.\n"+
+                              "# W wypadku, gdy istnieje wybór pomiędzy zbiciem \nróżnych ilości warcab przeciwnika, \nto obowiązkowe jest bicie większej ilości warcab.\n"+
+                              "# Jeżeli zwykły kamień w czasie bicia przechodzi \nprzez jedno z pól przemiany i kontynuuje bicie,\n to nie zamienia się w damkę i nadal \npozostaje kamieniem.\n"+
+                              "\n"+
+                              "3. Cel i rezultat gry\n"+
+                              "# Celem gry jest uniemożliwienie przeciwnikowi \nwykonania posunięcia poprzez zbicie lub\n zablokowanie jego warcab. \nW pozostałych przypadkach partię uznaje się \nza remisową.\n"+                              
+                              "\n"+
+                              "źródło: http://pl.wikipedia.org/wiki/Warcaby_klasyczne";
+                              
+                   pole1.setText(s);
+                   add(pole1);
+		}
 	}
 }
